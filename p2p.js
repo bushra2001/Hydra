f35d28de623644ce390c7990e4a3587cc0106a14
bushra2001 <bushra_ashfaq8@yahoo.com>
2022-02-24 23:22:11 +0500

    Adding comments in p2p.js

--- p2p.js
+++ p2p.js
@@ -1,21 +1,35 @@
+//impory statement for the open source libraries
+//using const instead of let; to ensure there is no rebinding
 const crypto = require('crypto'),
   Swarm = require('discovery-swarm'),
   defaults = require('dat-swarm-defaults'),
   getPort = require('get-port');
 
+//Set variables to hold an object with the peers and connection sequence
+
 const peers = {};
 let connSeq = 0;
+
+//Hydra is a channel name that all nodes connecting to
 let channel = 'Hydra';
 
+//set randomly generated peer ID for utilizing crypto library 
 const myPeerId = crypto.randomBytes(32);
 console.log('my PeerId: ' + myPeerId.toString('hex'));
 
+//Generate config object that holds peerID
 const config = defaults({
     id:myPeerId,
 });
 
+//Using config object to initialize swarm library
+//Swarm library create a network swarm that uses discovery-channel to find and connect peers on a UCP/TCP network
 const swarm = Swarm(config);
+
+//async function continuously monitor swarm on event messages
 (async () => {
+//Listen on random port selected and once a connection made to a peer
+// using setKeepAlive to ensure the network connection stays with other peers
     const port = await getPort();
     swarm.listen(port);
     console.log('Listening port: ' + port);
@@ -31,6 +45,8 @@ const swarm = Swarm(config);
                 console.log('exception', exception);
             }
         }
+//Once data message received on p2p network, parse data using JSON.parse; decoding msg back into an object
+//toString command converts bytes into readable string data type.
         conn.on('data', data => {
             let message = JSON.parse(data);
             console.log('----------- Received Message start------');
@@ -42,6 +58,7 @@ const swarm = Swarm(config);
             );
             console.log('---------Received Message end -------');
         });
+// Listening to close event, indicating lost connection with peers
         conn.on('close', () => {
             console.log(`Connection ${seq} closed, peerId:${peerId}`);
             if (peers[peerId].seq === seq){
@@ -56,10 +73,11 @@ const swarm = Swarm(config);
         connSeq++
     })
 })();
+//send msg after ten seconds to any available peers
 setTimeout(function(){
     writeMessageToPeers('hello', null);
 }, 10000);
-
+//writeMessageToPeers will be sending message to all connected pairs
 writeMessageToPeers = (type,data) => {
     for (let id in peers){
         console.log('-----writeMessageToPeers start--------');
@@ -68,7 +86,7 @@ writeMessageToPeers = (type,data) => {
         sendMessage(id, type, data);
     }
 };
-
+//writeMessageToPeerToId will be sending message to a specific peer ID
 writeMessageToPeerToId = (toId, type, data) => {
     for (let id in peers) {
         if (id === toId) {
@@ -79,6 +97,7 @@ writeMessageToPeerToId = (toId, type, data) => {
         }
     }
 };
+//Generic method to send a message formatted with params you would like to pass
 sendMessage = (id, type, data) => {
     peers[id].conn.write(JSON.stringify(
         {
